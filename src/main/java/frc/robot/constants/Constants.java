0b67d732c04d9664c3cafaa9a866e7bcf9a45597
madmathus <rhoyxu@gmail.com>
2026-09-14 17:12:25 -0700

    scaled to 0.1

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -7,7 +7,7 @@ import com.ctre.phoenix6.hardware.Pigeon2;
 
 public class Constants {
     public static final double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    public static final double scaling = 0.3;
+    public static final double scaling = 0.1;
 
     public static final int pigeonID = 15;
     public static final Pigeon2 imu = new Pigeon2(Constants.pigeonID);
